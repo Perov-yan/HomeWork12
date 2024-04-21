19122b08ac60a9dcdaf1a62284eb82e0316b1cef
Ян <peroff007@gmail.com>
2024-04-21 23:56:35 +0300

    Домашняя работа занятия Методы объектов

--- src/Author.java
+++ src/Author.java
@@ -15,4 +15,22 @@ public class Author {
         return lastName;
     }
 
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+        Author author = (Author) object;
+        return firstName.equals(firstName) && lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(firstName, lastName);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,19 @@ public class Main {
     public static void main(String[] args) {
         Author author1 = new Author("Сапковский", "Анджей");
         Author author2 = new Author("Ник", "Перумов");
+
         Book book1 = new Book("Ведьмак", author1, 2000);
         Book book2 = new Book("Дочь некроманта", author2, 2003);
 
         book1.setYearPublication(1998);
-        System.out.println(book1.getTitle() + " написал " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName() + ", опубликована " + book1.getYearPublication());
-        System.out.println(book2.getTitle() + " написал " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName() + ", опубликована " + book2.getYearPublication());
+
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
+        System.out.println(author2.equals(author2));
+        System.out.println(book2.equals(book2));
+        System.out.println(author1.hashCode());
+        System.out.println(book2.hashCode());
+
+
     }
 }
